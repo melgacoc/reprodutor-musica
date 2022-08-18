c5e8568e7aaa36c35957324ce88b1ec31d12fa34
Claudio Melgaco <claudio19a@hotmail.com>
2022-08-18 16:39:46 -0300

    req 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,13 +3,53 @@ import React from 'react';
 import Header from './Components/Header';
 
 class Search extends React.Component {
+  state = {
+    isSearchButtonDisabled: true,
+    searchField:'',
+  }
+
+  isSearchButtonDisabled = () => {
+    const {
+      searchField } = this.state;
+    const number = 2;
+    const verifyInput = searchField.length < number;
+
+    this.setState({
+      isSearchButtonDisabled: verifyInput,
+    });
+  }
+
+  searchButton = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => {
+      this.isSearchButtonDisabled();
+    });
+  }
+
   render() {
+    const {
+      searchField,
+      isSearchButtonDisabled,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <p>a</p>
         <div>
-          <input type="search" name="" id="" />
+          <input data-testid="search-artist-input"
+          type="search"
+          name="searchField"
+          value={ searchField }
+          id=""
+          placeholder='Nome do Artista'
+          onChange={ this.searchButton }
+          />
+          <button data-testid="search-artist-button"
+          disabled={ isSearchButtonDisabled}
+          onClick={ this.search }>
+            Pesquisar
+          </button>
         </div>
       </div>
     );
